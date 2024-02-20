6e17f15a4b542d0b55586c7c675dfa196117160b
ctorresm96 <carlostm320@gmail.com>
2024-02-19 23:02:22 -0500

    removed project

--- src/app/feature/bridge-pattern/bridge-pattern.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BridgePatternComponent } from './bridge-pattern.component';
-
-@NgModule({
-  declarations: [BridgePatternComponent],
-  imports: [CommonModule],
-})
-export class BridgePatternModule {}
